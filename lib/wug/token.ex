9397e125846232a4186e2ffa9f94532903f1e7ea
Matt Pruitt <matt@guitsaru.com>
2023-08-10 09:40:30 +0000

    Update token

--- lib/wug/token.ex
+++ lib/wug/token.ex
@@ -1,9 +1,17 @@
 defmodule Wug.Token do
+  @moduledoc "Represents a single parsed token"
+
   @type t :: %__MODULE__{
           start: non_neg_integer(),
           starts_sentence: boolean,
-          text: String.t()
+          text: String.t(),
+          end: non_neg_integer(),
+          type: :unk | :punct | :word | :space
         }
 
-  defstruct start: 0, starts_sentence: false, text: ""
+  defstruct start: 0, starts_sentence: false, text: "", end: 0, type: :unk
+
+  def new(index) do
+    %__MODULE__{start: index}
+  end
 end
